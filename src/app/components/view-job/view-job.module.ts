08a3a28623d84d4d730463fe8ad5550ed46dbd82
imasr <ashishs723@gmail.com>
2018-09-25 18:34:53 +0530

    job view page

--- src/app/components/view-job/view-job.module.ts
+++ src/app/components/view-job/view-job.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewJobComponent } from './view-job.component';
 import { Routes, RouterModule } from '@angular/router';
+import { MatCardModule, MatTabsModule, MatListModule } from '@angular/material';
 
 const routes: Routes = [
     {
@@ -12,7 +13,10 @@ const routes: Routes = [
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule.forChild(routes)
+        RouterModule.forChild(routes),
+        MatCardModule,
+        MatTabsModule,
+        MatListModule
     ],
     declarations: [ViewJobComponent]
 })
